ecb4524c461a177d09838bd4d41e1c2bc8fcc3e2
Erlangga Aditya Delfiana <erlangga.delfiana@digitalbdg.ac.id>
2026-04-20 17:07:43 +0700

    Fix redirect path for blocked sites

--- root.js
+++ root.js
@@ -1,16 +1,12 @@
 import { blockedSites } from './url.js';
 
-// Ambil parameter dari URL (?site=...)
 const params = new URLSearchParams(window.location.search);
 const site = params.get("site");
 
 if (site) {
-  console.log("Membuka:", site);
-
-  // Cek apakah termasuk blacklist
   const isBlocked = blockedSites.some(blocked => site.includes(blocked));
 
   if (isBlocked) {
-    window.location.href = "blocked.html";
+    window.location.href = "./blocked.html";
   }
 }
