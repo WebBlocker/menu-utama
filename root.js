ce6a7e70cca01c0b94ed8522ba62cd0bbfbd44da
Erlangga Aditya Delfiana <erlangga.delfiana@digitalbdg.ac.id>
2026-04-22 16:36:13 +0700

    Update root.js

--- root.js
+++ root.js
@@ -1,13 +1,25 @@
 import { blockedSites } from './url.js';
 
-const params = new URLSearchParams(window.location.search);
-const site = params.get("site");
+// ambil element (INI YANG DIMINTA DOSEN)
+const btnYoutube = document.getElementById("btnYoutube");
+const btnGoogle = document.getElementById("btnGoogle");
 
-if (site) {
-  const isBlocked = blockedSites.some(blocked => site.includes(blocked));
+// function handler
+function openSite(site) {
+  const isBlocked = blockedSites.some(b => site.includes(b));
 
   if (isBlocked) {
     window.location.href = "./blocked.html";
+  } else {
+    window.location.href = "https://" + site;
   }
 }
 
+// event listener (pakai element)
+btnYoutube.addEventListener("click", () => {
+  openSite("youtube.com");
+});
+
+btnGoogle.addEventListener("click", () => {
+  openSite("google.com");
+});
